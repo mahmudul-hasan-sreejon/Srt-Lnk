aea3e8926b590c435a8efe548e9bb579d769140f
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-07 14:46:03 +0600

    Router setup added

--- client/main.js
+++ client/main.js
@@ -1,18 +1,27 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { browserHistory } from 'react-router';
 
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
+import NotFound from './../imports/ui/NotFound';
+import Login from './../imports/ui/Login';
 
+
+const routes = (
+  <Router history={browserHistory}>
+    <Switch>
+      <Route path="/" exact component={Login}/>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/link" component={Link}/>
+      
+      <Route component={NotFound}/>
+    </Switch>
+  </Router>
+);
 
 Meteor.startup(() => {
-  const app = (
-    <div>
-      <Signup/>
-      <Link/>
-    </div>
-  );
-
-  ReactDOM.render(app, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
