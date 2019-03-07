b4ac1bce8f505b7318fe6369963e5762e295a56b
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-07 17:37:42 +0600

    Browser history object created

--- client/main.js
+++ client/main.js
@@ -1,14 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { browserHistory } from 'react-router';
+import { Router, Route, Switch } from 'react-router-dom';
+import browserHistory from './../imports/history';
 
 import Signup from './../imports/ui/Signup';
 import Link from './../imports/ui/Link';
 import NotFound from './../imports/ui/NotFound';
 import Login from './../imports/ui/Login';
 
+window.bh = browserHistory;
 
 const routes = (
   <Router history={browserHistory}>
@@ -16,7 +17,7 @@ const routes = (
       <Route path="/" exact component={Login}/>
       <Route path="/signup" component={Signup}/>
       <Route path="/link" component={Link}/>
-      
+
       <Route component={NotFound}/>
     </Switch>
   </Router>
