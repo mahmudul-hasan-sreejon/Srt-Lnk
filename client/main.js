25df0760ccd7c95f34a9da3d246b6108af29bf4d
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-10 02:13:15 +0600

    Page authentication added

--- client/main.js
+++ client/main.js
@@ -11,19 +11,38 @@ import NotFound from './../imports/ui/NotFound';
 import Login from './../imports/ui/Login';
 
 
-// window.b = browserHistory;
+window.bh = browserHistory;
 
 // user route access types
 const unauthenticatedPages = ['/', '/signup'];
 const authenticatedPages = ['/links'];
 
+const onEnterPublicPage = () => {
+  const pathname = browserHistory.location.pathname;
+
+  if(Meteor.userId()) { // if a user has logged in
+    browserHistory.replace('/links');
+    return <Link />;
+  }
+  else if(pathname === '/') return <Login />;
+  else if(pathname === '/signup') return <Signup />;
+};
+
+const onEnterPrivatePage = () => {
+  if(!Meteor.userId()) {
+    browserHistory.replace('/');
+    return <Login />;
+  }
+  else return <Link />;
+};
+
 // app routes
 const routes = (
   <Router history={browserHistory}>
     <Switch>
-      <Route path="/" exact component={Login}/>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/links" component={Link}/>
+      <Route exact path="/" render={onEnterPublicPage} />
+      <Route exact path="/signup" render={onEnterPublicPage} />
+      <Route exact path="/links" render={onEnterPrivatePage} />
 
       <Route component={NotFound}/>
     </Switch>
@@ -42,10 +61,10 @@ Tracker.autorun(() => {
 
   // check authentication and redirect accordingly
   if(isUnauthenticatedPage && isAuthenticated) { // if on unauthenticated page and user logged in
-    browserHistory.push('/links');
+    browserHistory.replace('/links');
   }
   else if(isAuthenticatedPage && !isAuthenticated) { // if on authenticated page and user not logged in
-    browserHistory.push('/');
+    browserHistory.replace('/');
   }
 });
 
