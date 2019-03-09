517b28c1a2c22adaa5e7abc77c643dca4be2a90f
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-09 22:50:12 +0600

    User authentication added

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
@@ -10,7 +11,13 @@ import NotFound from './../imports/ui/NotFound';
 import Login from './../imports/ui/Login';
 
 
-// all app routes
+// window.b = browserHistory;
+
+// user route access types
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
+
+// app routes
 const routes = (
   <Router history={browserHistory}>
     <Switch>
@@ -22,6 +29,25 @@ const routes = (
     </Switch>
   </Router>
 );
+
+// track user authentication status
+Tracker.autorun(() => {
+  // get user authentication status
+  const isAuthenticated = !!Meteor.userId();
+  // get current browser path
+  const pathname = browserHistory.location.pathname;
+  // get current page' authentication status
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  // check authentication and redirect accordingly
+  if(isUnauthenticatedPage && isAuthenticated) { // if on unauthenticated page and user logged in
+    browserHistory.push('/links');
+  }
+  else if(isAuthenticatedPage && !isAuthenticated) { // if on authenticated page and user not logged in
+    browserHistory.push('/');
+  }
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
