0173a822da434fb41f710d0dcffe64f3ef0a7789
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-11 19:21:33 +0600

    User links add functionality added

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base';
 import browserHistory from './../history';
 
+import { Links } from './../api/links';
+
 
 export default class Link extends React.Component {
   onLogout() {
@@ -9,12 +11,31 @@ export default class Link extends React.Component {
     Accounts.logout();
   }
 
+  onSubmit(e) {
+    // prevent browser page refresh
+    e.preventDefault();
+    // get user url
+    const url = this.refs.url.value.trim();
+
+    if(url) {
+      // insert url into database
+      Links.insert({ url });
+      // reset url ref.
+      this.refs.url.value = '';
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Your links...</h1>
-
         <button onClick={this.onLogout.bind(this)}>Logout</button>
+
+        <p>Add a Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type='text' ref='url' placeholder='url' />
+          <button>Add Link</button>
+        </form>
       </div>
     );
   }
