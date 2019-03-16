8fcdf3fbaa34ce9030171dc7f9074a51701e81a1
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-17 02:47:47 +0600

    Middleware for user link redirect added

--- server/main.js
+++ server/main.js
@@ -1,11 +1,28 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import './../imports/startup/simple-schema-config';
 
 import './../imports/api/users';
-import './../imports/api/links';
+import { Links } from './../imports/api/links';
 
 
 Meteor.startup(() => {
-
+  // check current pages' connect handlers in the middleware
+  WebApp.connectHandlers.use((req, res, next) => {
+    // get the id form the url
+    const _id = req.url.slice(1);
+    // find the url _id
+    const link = Links.findOne({ _id });
+    
+    if(link) {
+      // set http status code
+      res.statusCode = 302;
+      // set http headers to redirect it's location
+      res.setHeader('Location', link.url);
+      // end http request
+      res.end();
+    }
+    else next();
+  });
 });
