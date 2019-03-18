e2ed35256c9c1e030e70cfb1499425c0dddfc228
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-18 19:43:01 +0600

    Links copy to clipboard functionality added

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,18 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ClipboardJS from 'clipboard';
 
-const LinksListItem = (props) => {
-  const url = props.url;
-  const shortUrl = props.shortUrl;
+
+export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isCopied: false
+    };
+  }
   
-  return (
-    <div>
-      <p>Url: {url}</p>
-      <p>Short Url: {shortUrl}</p>
-    </div>
-  );
-};
+  componentDidMount() {
+    // copy all text from new clipboard
+    this.clipboard = new ClipboardJS(this.refs.copy);
+
+    this.clipboard.
+    on('success', () => {
+      // update isCopied state
+      this.setState({ isCopied: true });
+
+      // update isCopied state after 1.2 sec
+      setTimeout(() => this.setState({ isCopied: false }), 1200);
+    }).
+    on('error', () => {
+      alert('Unable to copy');
+    });
+  }
+
+  componentWillUnmount() {
+    // destroy clipboard object
+    this.clipboard.destroy();
+  }
+
+  render() {
+    const url = this.props.url;
+    const shortUrl = this.props.shortUrl;
+
+    return (
+      <div>
+        <p>Url: {url}</p>
+        <p>Short Url: {shortUrl}</p>
+
+        <button ref="copy" data-clipboard-text={shortUrl}>
+          {this.state.isCopied ? 'Copied' : 'Copy'}
+        </button>
+      </div>
+    );
+  }
+}
 
 LinksListItem.propTypes = {
   _id: PropTypes.string.isRequired,
@@ -20,5 +58,3 @@ LinksListItem.propTypes = {
   userId: PropTypes.string.isRequired,
   shortUrl: PropTypes.string.isRequired
 };
-
-export default LinksListItem;
