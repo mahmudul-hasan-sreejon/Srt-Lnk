66534042eda86806cb1f651d90327619e6a060b9
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-20 03:33:06 +0600

    Updated LinksListFilters checkbox to controlled input field

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import { Session } from 'meteor/session';
+import { Tracker } from 'meteor/tracker';
 
-export default () => {
-  const checkVisibility = (e) => {
+
+export default class LinksListFilters extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showVisible: false
+    };
+  }
+
+  componentDidMount() {
+    // start a tracker for visibility check
+    this.linksListFiltersTracker = Tracker.autorun(() => {
+      this.setState({ showVisible: Session.get('showVisible') });
+    });
+  }
+
+  componentWillUnmount() {
+    // stop tracker on component unmount
+    this.linksListFiltersTracker.stop();
+  }
+
+  checkVisibility(e) {
     const status = e.target.checked;
 
     // update session variable
     Session.set('showVisible', !status);
-  };
+  }
 
-  return (
-    <div>
-      <label>
-        <input type="checkbox" onChange={checkVisibility} />
-        show hidden links
-      </label>
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={!this.state.showVisible} onChange={this.checkVisibility.bind(this)} />
+          show hidden links
+        </label>
+      </div>
+    );
+  }
+}
