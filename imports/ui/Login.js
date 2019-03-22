223ed3c9e50bef93849d728c626d3a3122ec64ea
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-22 17:30:59 +0600

    boxed-view styles applied to login and signup component

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -30,19 +30,21 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Srt Lnk</h1>
+      <div className='boxed-view'>
+        <div className='boxed-view__box'>
+          <h1>Srt Lnk</h1>
 
-        {this.state.error ? <p>{this.state.error}</p> : undefined}
+          {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)} noValidate>
-          <input type="email" ref="email" name="email" placeholder="Email" />
-          <input type="password" ref="password" name="password" placeholder="Password" />
+          <form onSubmit={this.onSubmit.bind(this)} noValidate>
+            <input type="email" ref="email" name="email" placeholder="Email" />
+            <input type="password" ref="password" name="password" placeholder="Password" />
 
-          <button>Login</button>
-        </form>
+            <button>Login</button>
+          </form>
 
-        <Link to='/signup'>Have an account?</Link>
+          <Link to='/signup'>Have an account?</Link>
+        </div>
       </div>
     );
   }
