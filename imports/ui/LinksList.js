d38ac9d0834d2a6783f91a42aaf7ad2fe1f5047a
Mahmudul Hasan Sreejon <mahmudul.hasan.sreejon@gmail.com>
2019-03-23 20:09:44 +0600

    FlipMove added in LinksList components

--- imports/ui/LinksList.js
+++ imports/ui/LinksList.js
@@ -3,6 +3,8 @@ import { Tracker } from 'meteor/tracker';
 import { Meteor } from 'meteor/meteor';
 import { Session } from 'meteor/session';
 
+import FlipMove from 'react-flip-move';
+
 import { Links } from './../api/links';
 
 import LinksListItem from './LinksListItem';
@@ -58,9 +60,9 @@ export default class LinksList extends React.Component {
   render() {
     return (
       <div>
-        <div>
+        <FlipMove maintainContainerHeight={true}>
           {this.renderLinksListItems()}
-        </div>
+        </FlipMove>
       </div>
     );
   }
